67d777f63bf316ee453394331246196b1f2bff49
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2020-07-07 10:12:53 -0500

    Add files via upload

---
+++ Examples/Surrogate Modeling/templatedir/test.sh
@@ -0,0 +1,46 @@
+ # copyright (c) 2020 ASandip
+ #This program is free software: you can redistribute it and/or 
+ # modify it under the terms of the GNU General Public License as 
+ # published by the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ 
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+#!/bin/bash
+
+#Source EOF library environment
+. $HOME/EOF-Library/etc/bashrc
+
+#Source OpenFoam environment
+source ~/openfoam6/etc/bashrc
+
+
+#Compile EOF Library
+eofCompile >> log.out 2>&1
+
+#Compile the chosen solver for the test, mhdInterFoam6
+wmake ../Solvers/mhdInterFoam6 >> log.out 2>&1 
+
+
+#Copy Elmer and OpenFoam case files to Dakota Gui workspace
+cp -r ../casebase/* .
+
+#Transfer new set of values for the chosen input parameters to the case files 
+cp transportProperties constant/transportProperties
+cp case_d.sif case.sif
+
+#Prepare test
+setFields  >> log.out 2>&1
+decomposePar >> log.out 2>&1
+ElmerGrid 2 2 meshElmer -metis 2
+
+
+#Run openFoam on 2 processes and Elmer on 2 processes
+mpirun --allow-run-as-root  -np 2 mhdInterFoam -parallel : -np 1 ElmerSolver_mpi case.sif >> log.out 2>&1
---
+++ Examples/UQ/README.md
@@ -0,0 +1,17 @@
+
+The benchmark case [1] was extended to a UQ study.
+
+    Data analysis: Uncertainty quantification/Latin hypercube sampling study #of samples: 100 https://dakota.sandia.gov/sites/default/files/docs/6.10/html-ref/method-sampling-sample_type-lhs.html
+    Input parameter/s: melt density (2500 +/- 2000 kg/m3), surface tension (5.1 +/- 4.9 N/m), ac frequency (21250 +/- 18750 Hz), current density (5e7 +/- 3e7 A/m2)
+    Output parameter/s: melt shape (Lorentz force and flow velocity) at the final timestep
+    The turbulence model chosen for this study was k − ω SST.
+    More details on the test case (boundary conditions, etc.), can be found here [2]
+
+References: 
+[1]. Spitans, Sergejs, Egbert Baake, Bernard Nacke, and Andris Jakovics. "Numerical modeling of free surface dynamics of melt in an alternate electromagnetic field. Part II: Conventional electromagnetic levitation." Metallurgical and Materials Transactions B 47, no. 1 (2016): 522-536. 
+[2]. Vencels, J., A. Jakovics, and V. Geza. "Simulation of 3D MHD with free surface using open-source EOF-Library: levitating liquid metal in an alternating electromagnetic field." Magnetohydrodynamics 53, no. 4 (2017): 643-652.
+
+
+
+
+
